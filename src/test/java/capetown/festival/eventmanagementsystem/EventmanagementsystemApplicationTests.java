1bd842d70d2b886aaa5ccb55666af799db029523
Kabelo <masolakabelo@gmail.com>
2025-02-06 21:51:40 +0200

    first commit

---
+++ src/test/java/capetown/festival/eventmanagementsystem/EventmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package capetown.festival.eventmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
